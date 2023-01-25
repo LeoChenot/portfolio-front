145a9c178947dcf1325dc9d8cd05703674d48140
LeoChenot <leochenot1@gmail.com>
2023-01-25 22:16:01 +0100

    footer done

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,4 +1,6 @@
+/* eslint-disable max-len */
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 // import PropTypes from 'prop-types';
 import './style.scss';
@@ -6,7 +8,34 @@ import './style.scss';
 function Footer() {
   return (
     <footer className="footer">
-      footer
+      <div className="footer__information">
+
+        <div className="footer__information__column">
+          <div className="footer__information__column__title">Portfolio</div>
+          <div className="footer__information__column__content">
+            Voici mon portfolio, il contient une liste de mes projets personnels ainsi que le moyen de me contacter.
+          </div>
+        </div>
+        <div className="footer__information__column">
+          <div className="footer__information__column__title">Liens utiles</div>
+          <div className="footer__information__column__content">
+            <div className="footer__information__column__content__links">
+              <Link className="footer__information__column__content__links__link" to="/home">Accueil</Link>
+              <Link className="footer__information__column__content__links__link" to="/home#presentation">Présentation</Link>
+              <Link className="footer__information__column__content__links__link" to="/home#projects">Mes projets</Link>
+              <Link className="footer__information__column__content__links__link" to="/home#contact">Contact</Link>
+            </div>
+          </div>
+        </div>
+
+      </div>
+      <div className="footer__copyright">
+        &copy;
+        {' '}
+        {new Date().getFullYear()}
+        {' '}
+        Copyright: leochenot.com
+      </div>
     </footer>
   );
 }
