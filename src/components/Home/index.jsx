18bb5bdafd11ea0a455cfae93f2d65b38aeec17c
LeoChenot <leochenot1@gmail.com>
2023-01-30 17:32:49 +0100

    add scrollBackToTopButton

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,17 +1,55 @@
-import React from 'react';
+import React, {
+  useEffect, useContext, useState, useRef,
+} from 'react';
+import { IoMdArrowUp } from 'react-icons/io';
 import Contact from '../Contact';
 import Presentation from '../Presentation';
 import Projects from '../Projects';
+
+import { ScrollContext } from '../ScrollProvider';
 
 // import PropTypes from 'prop-types';
 import './style.scss';
 
 function Home() {
+  const scrollYContext = useContext(ScrollContext);
+  const [scrollY, setScrollY] = useState(undefined);
+
+  const scrollToTopButtonRef = useRef();
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+    });
+  };
+
+  useEffect(() => {
+    if (scrollY >= 56) {
+      if (scrollToTopButtonRef.current.classList.contains('home__scrollTopButton__hidden')) {
+        scrollToTopButtonRef.current.classList.remove('home__scrollTopButton__hidden');
+      }
+    }
+    else if (!scrollToTopButtonRef.current.classList.contains('home__scrollTopButton__hidden')) {
+      scrollToTopButtonRef.current.classList.add('home__scrollTopButton__hidden');
+    }
+  }, [scrollY]);
+
+  // Ici, comme le scrollYContext arrive avant que le composant Home soit chargé
+  // Je le met donc dans un state local
+  useEffect(() => {
+    if (scrollYContext) {
+      setScrollY(scrollYContext);
+    }
+  }, [scrollYContext]);
+
   return (
     <div className="home">
       <Presentation />
       <Projects />
       <Contact />
+      <button className="home__scrollTopButton home__scrollTopButton__hidden" ref={scrollToTopButtonRef} type="button" onClick={scrollToTop}>
+        <IoMdArrowUp className="home__scrollTopButton__icon" />
+      </button>
     </div>
   );
 }
